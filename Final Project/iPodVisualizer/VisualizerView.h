83c7d409b0b225e5e3dadbc881a7756144c669d2
oLeThiVanAnh <le.thi.van.anh@framgia.com>
2018-04-05 14:41:00 +0700

    Report 3/2018

---
+++ Final Project/iPodVisualizer/VisualizerView.h
@@ -0,0 +1,14 @@
+//
+//  VisualizerView.h
+//  iPodVisualizer
+//
+//  Created by Le Thi Van Anh on 3/9/18.
+//  Copyright © 2018 Xinrong Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface VisualizerView : UIView
+@property (strong, nonatomic) AVAudioPlayer *audioPlayer;
+@end
